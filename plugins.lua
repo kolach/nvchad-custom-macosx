d701954f53477f992c3fb32f64de0582aeefc19b
kolach <chikolad@gmail.com>
2023-10-08 12:24:02 -0500

    add chatgpt ai plugin

--- plugins.lua
+++ plugins.lua
@@ -121,6 +121,20 @@ local plugins = {
       return require "custom.configs.formatter"
     end
   },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function ()
+      require("chatgpt").setup({
+        api_key_cmd = "op read op://Private/gpt_api_key_nvim/credential",
+      })
+    end
+  },
 }
 
 return plugins
